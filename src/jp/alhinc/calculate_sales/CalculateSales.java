30fcc58e98c8dcc8080b62e00825110e3a1c4751
kamei_ayako <kamei.ayako@alhinc.jp>
2025-06-11 16:47:22 +0900

    処理内容2‐2　型の変換

--- src/jp/alhinc/calculate_sales/CalculateSales.java
+++ src/jp/alhinc/calculate_sales/CalculateSales.java
@@ -50,14 +50,72 @@ public class CalculateSales {
 			//rcdかどうか確認する
 			//8桁かどうかを確認する
 			//一致したら(trueの場合)リストに入れる、一致しなかったら(False)何もしない（51行目に進む、if文の中に入らない）
-			if(files[i].getName().matches("^\\d{8}$.rcd")) {
+			if(files[i].getName().matches("^\\d{8}.rcd$")) {
 			    //リストに入れる。売上ファイルの条件に当てはまったものだけ、List(ArrayList) に追加します。
 				rcdFiles.add(files[i]);
 			}
-
-
-			//files[i].getName()
 		}
+
+
+		//rcdFilesに複数の売上ファイルの情報を格納しているので、その数だけ繰り返します。
+		for(int i = 0; i < rcdFiles.size(); i++) {
+
+			//支店定義ファイル読み込み(readFileメソッド)を参考に売上ファイルの中身を読み込みます。
+			//売上ファイルの1行目には支店コード、2行目には売上金額が入っています。
+			BufferedReader br = null;
+
+			try {
+				//ファイルを開く
+				File file = new File(args[0], rcdFiles.get(i).getName());
+				FileReader fr = new FileReader(file);
+				br = new BufferedReader(fr);
+
+				//lineの使い道…読み込んだ直後の一旦の受け皿
+				String line;
+
+				//fileContentsの使い道…この後の足し算等に使用
+				List<String> fileContents = new ArrayList<>();
+
+				// readlineメソッドを使って一行読んだら、lineに入れる(代入)
+				while((line = br.readLine()) != null) {
+						//lineに入っている文字列を、リストに格納（add）
+					fileContents.add(line);
+				}
+
+				//売上ファイルから読み込んだ売上金額をMapに加算していくために、型の変換を行います。
+				//※詳細は後述で説明
+				long fileSale = Long.parseLong(fileContents.get(1));
+
+				//読み込んだ売上⾦額を加算します。
+				//※詳細は後述で説明
+				Long saleAmount = branchSales.get(fileContents.get(0)) + fileSale;
+
+				//加算した売上⾦額をMapに追加します。
+
+
+			} catch(IOException e) {
+				System.out.println(UNKNOWN_ERROR);
+				return;
+			} finally {
+				// ファイルを開いている場合
+				if(br != null) {
+					try {
+						// ファイルを閉じる
+						br.close();
+					} catch(IOException e) {
+						System.out.println(UNKNOWN_ERROR);
+						return;
+					}
+				}
+			};
+		}
+
+
+
+
+
+
+
 
 
 
@@ -81,17 +139,20 @@ public class CalculateSales {
 	 * @return 読み込み可否
 	 */
 	private static boolean readFile(String path, String fileName, Map<String, String> branchNames, Map<String, Long> branchSales) {
+		//変数の宣言
 		BufferedReader br = null;
 
 		try {
+			//ファイルを開く
 			File file = new File(path, fileName);
 			FileReader fr = new FileReader(file);
 			br = new BufferedReader(fr);
 
 			String line;
-			// 一行ずつ読み込む
+			// readlineメソッドを使って一行読んだら、lineに入れる(代入)
 			while((line = br.readLine()) != null) {
 				// ※ここの読み込み処理を変更してください。(処理内容1-2)
+				//読んだもの（line）をスプリッドして（カンマ,を境に）、itemsに入れる
 				String[] items = line.split(",");
 
 				 branchNames.put(items[0], items[1]);
